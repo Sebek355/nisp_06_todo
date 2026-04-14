7c02a52d41179c5a5f937f7c8a1d3fb626168da2
Sebastian Swiatlowski <sebastianswiatlowski1@gmail.com>
2026-04-14 12:39:20 +0200

    feat: Dodano usuwanie zadan

--- app.js
+++ app.js
@@ -2,14 +2,40 @@ const input = document.getElementById("task-input");
 const button = document.getElementById("add-btn");
 const list = document.getElementById("task-list");
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function () {
   const taskText = input.value.trim();
 
   if (taskText !== "") {
     const li = document.createElement("li");
-    li.textContent = taskText;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "✕";
+    deleteBtn.classList.add("delete-btn");
+
+    checkbox.addEventListener("change", function () {
+      span.classList.toggle("done");
+    });
+
+    deleteBtn.addEventListener("click", function () {
+      li.classList.add("removing");
+
+      setTimeout(() => {
+        li.remove();
+      }, 300);
+    });
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
     list.appendChild(li);
 
-    input.value = ""; // czyści input
+    input.value = "";
   }
 });
